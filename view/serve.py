5e95b8ceff32c18ac6027f1e3660a0cd512259b9
Daniel Borlay <daniel.borlay@ericsson.com>
2017-02-12 13:44:56 +0100

    Add new serving module as view
    Signed-off-by: Daniel Borlay <daniel.borlay@ericsson.com>

---
+++ view/serve.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""
+ This module serves the HTTP requests for the application.
+"""
+
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run()
