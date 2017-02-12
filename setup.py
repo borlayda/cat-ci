b32c625ee030d871bfab0b5c1e3f59d432fae827
Daniel Borlay <daniel.borlay@ericsson.com>
2017-02-12 13:44:26 +0100

    Add Python module installer file
    Signed-off-by: Daniel Borlay <daniel.borlay@ericsson.com>

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""
+ Installation module of the cat-ci application.
+"""
+
+from setuptools import setup
+
+setup(name='cat-ci',
+      version='0.0.1',
+      description=('CI framework for those, who are lazy as cats.'),
+      url='N/A',
+      author='Daniel Borlay',
+      author_email='borlay.daniel@gmail.com',
+      license='Proprietary',
+      packages=['cat-ci'],
+      zip_safe=False)
